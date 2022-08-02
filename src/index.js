8ae3f87e3564c4f9c5f1868033288c867d42c9b8
Somtochukwu <somtochukwuuchegbu@gmail.com>
2022-08-02 22:32:18 +0100

    add: added the POST request endpoint and default express JSON middleware

--- src/index.js
+++ src/index.js
@@ -3,19 +3,31 @@ const express = require("express");
 const app = express();
 const PORT = 3001
 
+// JSON express middleware
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`Running express server on port: ${PORT}`));
 
+const userInfo = [
+    {
+        name: 'somto',
+        age: 19
+    },
+    {
+        name: 'chibueze',
+        age: 21
+    }
+]
 
-app.get('/groceries', (request, response) => {
-    response.send([
-        {
-            name: 'somto',
-            age: 19
-        },
-        {
-            name: 'chibueze',
-            age: 21
-        }
-    ]);
+// GET request
+app.get('/users', (request, response) => {
+    response.send(userInfo);
 })
+
+// POST request
+app.post('/add-user', (request, response) => {
+    console.log(request.body);
+    userInfo.push(request.body);
+    response.send(201);
+});
 
